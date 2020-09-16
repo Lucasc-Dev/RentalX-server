17cc9c8116df1054af4194da67c73a96a6b6c974
Lucas César <lucasambrogi1@hotmail.com>
2020-09-15 23:40:52 -0300

    Routes file created.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import routes from './routes/routes';
 
 const app = express();
+
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
