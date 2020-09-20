41558605605745993ea4ffe12868b6b2928e629f
Lucas César <lucasambrogi1@hotmail.com>
2020-09-20 19:10:25 -0300

    Added an Error Handler.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import ErrorHandler from './middlewares/ErrorHandler';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -10,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(ErrorHandler);
 
 app.listen(3333, () => {
     console.log('Server running on port 3333');
