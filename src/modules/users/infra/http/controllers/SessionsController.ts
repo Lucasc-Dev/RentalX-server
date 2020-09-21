d08c6726964a681ac3502d5ba4de44d0e6b43d7c
Lucas César <lucasambrogi1@hotmail.com>
2020-09-21 01:12:30 -0300

    Created sessions route and its controller.

---
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export default class SessionsController {
+    public async create(request: Request, response: Response): Promise<Response> {
+        const { email, password } = request.body;
+
+        return response.json({ email, password });
+    }
+}
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import profilesRouter from '@modules/users/infra/http/routes/profile.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/profile', profilesRouter);
+router.use('/sessions', sessionsRouter);
 
 export default router;
