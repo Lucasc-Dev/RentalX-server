170894e28105048e230a07b3acde97f05d057f7b
Lucas César <lucasambrogi1@hotmail.com>
2020-09-23 23:40:52 -0300

    Upload system with multer.

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from '@config/upload';
 
 import AvatarController from '../controllers/AvatarController';
 import ProfilesController from '../controllers/ProfileController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const router = Router();
+const upload = multer(multerConfig.multer);
 
 const profilesController = new ProfilesController();
 const avatarController = new AvatarController();
@@ -13,6 +17,6 @@ router.use(ensureAuthenticated);
 
 router.get('/', profilesController.show);
 router.put('/', profilesController.update);
-router.patch('/avatar', avatarController.update);
+router.patch('/avatar', upload.single('avatar'), avatarController.update);
 
 export default router;
