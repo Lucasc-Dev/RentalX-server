c87b6c3b022b70b27f3d7e97e894cdd596d1f808
Lucas César <lucasambrogi1@hotmail.com>
2020-09-23 23:56:57 -0300

    Now uploaded files can be accessed by a link.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,11 +3,14 @@ import crypto from 'crypto';
 import multer from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+const uploadFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
 export default {
     multer: {
+        tempFolder: tmpFolder,
+        uploadFolder: uploadFolder,
+
         storage: multer.diskStorage({
-            destination: tmpFolder,
             filename(request, file, callback) {
                 const filehash = crypto.randomBytes(10).toString('hex');
                 const filename = `${filehash}-${file.originalname.split(' ').join('-')}`
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import 'express-async-errors';
 
 import routes from './routes';
+import uploadConfig from '@config/upload';
 import ErrorHandler from './middlewares/ErrorHandler';
 
 import '@shared/infra/typeorm';
@@ -11,6 +12,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use('/files', express.static(uploadConfig.multer.uploadFolder));
 app.use(express.json());
 app.use(routes);
 
