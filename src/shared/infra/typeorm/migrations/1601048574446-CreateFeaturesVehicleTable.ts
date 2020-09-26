fab8f72bc80a725dabc29cb544aae28d54c2d195
Lucas César <lucasambrogi1@hotmail.com>
2020-09-25 23:46:54 -0300

    CreateFeaturesVehicleTable migration created.

---
+++ src/shared/infra/typeorm/migrations/1601048574446-CreateFeaturesVehicleTable.ts
@@ -0,0 +1,39 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateFeaturesVehicleTable1601048574446 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'featureshehicle',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'car_id',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'feature_id',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'info',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('featuresvehicle')
+    }
+
+}
