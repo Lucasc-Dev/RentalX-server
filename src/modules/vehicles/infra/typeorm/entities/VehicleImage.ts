37ad4ccae4e832c63c70df0f118c1bce43839019
Lucas César <lucasambrogi1@hotmail.com>
2020-11-17 23:55:16 -0300

    VehicleImage entity: created.

---
+++ src/modules/vehicles/infra/typeorm/entities/VehicleImage.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Expose } from "class-transformer";
+import Vehicle from "./Vehicle";
+
+@Entity()
+export default class VehicleImage {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    vehicle_id: string;
+
+    @Column()
+    image: string;
+
+    @ManyToOne(type => Vehicle, vehicle => vehicle.images)
+    @JoinColumn({ name: 'vehicle_id' })
+    vehicle: Vehicle[];
+
+    @Expose({ name: 'image_url' })
+    getImageUrl(): string {
+        return `${process.env.UPLOAD_LINK}${this.image}`;
+    }
+}
