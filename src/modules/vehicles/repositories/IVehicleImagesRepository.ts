e54659484e7b6030ad90e412acf796c5c75f09e3
Lucas César <lucasambrogi1@hotmail.com>
2020-11-21 23:42:07 -0300

    VehicleImages repository: deleted.

--- src/modules/vehicles/repositories/IVehicleImagesRepository.ts
+++
@@ -1,6 +0,0 @@
-import VehicleImage from "../infra/typeorm/entities/VehicleImage";
-
-export default interface IVehicleImagesRepository {
-    save(data: VehicleImage): Promise<void>;
-    create(data: VehicleImage): Promise<VehicleImage>;
-}
